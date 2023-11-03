f98cdb463a86a517a2347f49ede58db2b8f91398
Alberto Sierra <Sierra.Alberto@iCloud.com>
2023-11-03 15:28:32 -0700

    start property for Author class

--- libs/models/author.py
+++ libs/models/author.py
@@ -1,7 +1,42 @@
 class Author:
     all = []
     def __init__(self, first_name, last_name, saved_madlibs):
+        self._first_name = None
+        self._last_name = None
+        
         self.first_name = first_name
         self.last_name = last_name
         self.saved_madlibs = saved_madlibs
         type(self).all.append(self)
+        
+    @property
+    def first_name(self):
+        return self._first_name
+    
+    @first_name.setter
+    def first_name(self,first_name):
+        if not isinstance (first_name, str) and 1<=len(first_name)<=15:
+            raise TypeError('First name must be a string between 1 and 15 characters long.')
+        else: 
+            self._first_name = first_name
+    @property
+    def last_name(self):
+        return self._last_name
+    
+    @last_name.setter
+    def last_name(self,last_name):
+        if not isinstance (last_name, str) and 1<=len(last_name)<=15:
+            raise TypeError('Last name must be a string between 1 and 15 characters long.')
+        else: 
+            self._last_name = last_name
+            
+    @property
+    def saved_madlibs(self):
+        return self._saved_madlibs
+    
+    @saved_madlibs.setter
+    def saved_madlibs(self, madlibs):
+        if not isinstance(madlibs, list) #? type madlibs class or list?
+            raise TypeError('Madlib must be a list')
+        else:
+            self._saved_madlibs = madlibs
