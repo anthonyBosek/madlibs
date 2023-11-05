40531144430f7d17220cee7b562d2d206f97e66f
Alberto Sierra <Sierra.Alberto@iCloud.com>
2023-11-05 12:04:23 -0800

    author property

--- libs/models/author.py
+++ libs/models/author.py
@@ -3,6 +3,7 @@ class Author:
     def __init__(self, first_name, last_name, saved_madlibs):
         self._first_name = None
         self._last_name = None
+        self._saved_madlibs = None
         
         self.first_name = first_name
         self.last_name = last_name
@@ -15,7 +16,7 @@ class Author:
     
     @first_name.setter
     def first_name(self,first_name):
-        if not isinstance (first_name, str) and 1<=len(first_name)<=15:
+        if not isinstance (first_name, str) or not 1<=len(first_name)<=15:
             raise TypeError('First name must be a string between 1 and 15 characters long.')
         else: 
             self._first_name = first_name
@@ -25,7 +26,7 @@ class Author:
     
     @last_name.setter
     def last_name(self,last_name):
-        if not isinstance (last_name, str) and 1<=len(last_name)<=15:
+        if not isinstance (last_name, str) or not 1<=len(last_name)<=15:
             raise TypeError('Last name must be a string between 1 and 15 characters long.')
         else: 
             self._last_name = last_name
@@ -36,7 +37,10 @@ class Author:
     
     @saved_madlibs.setter
     def saved_madlibs(self, madlibs):
-        if not isinstance(madlibs, list) #? type madlibs class or list?
+        if not isinstance(madlibs, list): #? type madlibs class or list?
             raise TypeError('Madlib must be a list')
         else:
             self._saved_madlibs = madlibs
+            
+    # @classmethod
+    # def drop_table():
